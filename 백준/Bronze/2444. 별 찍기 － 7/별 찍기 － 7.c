c731412d5c8bafbe3e9888797b464725dc341617
Hyuny_B <changhyeonb533@gmail.com>
2025-02-18 17:23:44 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	
+	for(int i = 0; i < N; i++){
+		for(int j = N-i-1; j > 0 ; j--){
+			printf(" ");
+		}
+		for(int j = 0; j < 2*i+1; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < i+1 ; j++){
+			printf(" ");
+		}
+		for(int j = 2*(N-1)-1-2*i; j > 0; j--){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
